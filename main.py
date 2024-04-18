2a4a42acd172403b42bc29f19e990dee93ec4c61
@PyAdminRU <zh.vitaliy92@gmail.com>
2024-04-18 15:58:06 +0300

    Подготовка документа для заполнения

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from docxtpl import DocxTemplate
 
-doc = DocxTemplate("шаблон.docx")
+doc = DocxTemplate("template/Трудовой_договор_с_работником.docx")
 context = { 'director' : "И.И.Иванов"}
 doc.render(context)
 doc.save("шаблон-final.docx")
