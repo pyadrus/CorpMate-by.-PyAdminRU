1e88af0b07fddd9cf44887683bf543a64946dfcd
@PyAdminRU <zh.vitaliy92@gmail.com>
2024-04-18 16:09:32 +0300

    Выделение вставленного текста (желтым цветом)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from docxtpl import DocxTemplate
 
 doc = DocxTemplate("template/Трудовой_договор_с_работником.docx")
-context = { 'director' : "И.И.Иванов"}
+context = {'name_surname': "<span style='color: yellow'>Жабинский В.В.</span>"}
 doc.render(context)
 doc.save("шаблон-final.docx")
